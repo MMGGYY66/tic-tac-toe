b4bf442597e709fe6b81d166e1d1661f538794a1
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-02-17 15:25:05 -0600

    add logic to the game

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,23 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import Square from "./Square";
 
 const Board = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const handleSquareClick = (position) => {
+    if (board[position]) {
+      return;
+    }
+    setBoard(prev => {
+      return prev.map((square, index) => {
+        if (index === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return square;
+      });
+    });
+    setIsXNext(prev => !prev);
+  };
+
+  const renderSquarePos = (position) => {
+    return (<Square
+      value={board[position]}
+      onClick={() =>
+        handleSquareClick(position)
+      }
+    />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+        {renderSquarePos(0)}
+        {renderSquarePos(1)}
+        {renderSquarePos(2)}
       </div>
       <div className="board-row">
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+        {renderSquarePos(3)}
+        {renderSquarePos(4)}
+        {renderSquarePos(5)}
       </div>
       <div className="board-row">
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+        {renderSquarePos(6)}
+        {renderSquarePos(7)}
+        {renderSquarePos(8)}
       </div>
     </div>
   )
