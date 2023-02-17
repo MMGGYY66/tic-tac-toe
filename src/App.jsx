7f56656552e5d913f1c780b7c1b6b8509a17d9ff
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-02-17 12:18:28 -0600

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Board from "./components/Board.jsx";
+import './styles/root.scss';
 
 export default () => (
-  <div>
+  <div className="app">
     <h1>Welcome to Tic Tac Toe Game!</h1>
     <Board />
   </div>
